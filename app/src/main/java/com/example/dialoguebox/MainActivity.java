7c4b41a5acdeec81e78ef1053e814a9b7d678882
basilmuhammad91 <basilmuhammad91@gmail.com>
2021-12-06 12:57:28 +0500

    Alert Dialog box

---
+++ app/src/main/java/com/example/dialoguebox/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dialoguebox;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button confirm_btn;
+    AlertDialog.Builder builder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        confirm_btn = findViewById(R.id.confirm_btn);
+        builder = new AlertDialog.Builder(this);
+
+        confirm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.setTitle("Title")
+                        .setMessage("Do You want to close this application?")
+                        .setCancelable(true)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+}
